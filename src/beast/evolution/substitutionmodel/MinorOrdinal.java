0669923213c3dd5f5d2b024eb09d3c707798c14a
Chi Zhang <zhangchicool@gmail.com>
2018-06-27 10:32:15 +0200

    modified ordinal

---
+++ src/beast/evolution/substitutionmodel/MinorOrdinal.java
@@ -0,0 +1,44 @@
+package beast.evolution.substitutionmodel;
+
+import beast.core.Citation;
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.parameter.RealParameter;
+
+/**
+ * @author Chi Zhang
+ */
+
+@Description("")
+public class MinorOrdinal extends NStatesNoRatesSubstitutionModel {
+
+    public final Input<RealParameter> bInput = new Input<>("b", "nonadjacent state change relative rate. b=0:ordered; b>0:unordered");
+
+    @Override
+    protected void setupRelativeRates() {
+
+        /* A relative rate matrix for a model of this class looks as follows (for, e.g. 5 states)
+         * [   1 b b b ]
+         * [ 1   1 b b ]
+         * [ b 1   1 b ]
+         * [ b b 1   1 ]
+         * [ b b b 1   ]
+         */
+
+        double b = 1.0;
+        if (bInput.get() != null) {
+            b = bInput.get().getValue();
+            if (b < 0.0)
+                throw new RuntimeException("relative rate b should be positive.");
+        }
+
+        for (int i = 0; i < nrOfStates - 1; i++) {
+            for (int j = 0; j < nrOfStates; j++) {
+                if (j == 0 || j == nrOfStates - 1)
+                    relativeRates[nrOfStates * i + j] = 1.0;
+                else
+                    relativeRates[nrOfStates * i + j] = b;
+            }
+        }
+    }
+}
